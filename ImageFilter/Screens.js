1e719ba3089cc07d58822a4b3bdff8dc26748d4f
Fedja Tica <fedja.tica@gmail.com>
2018-05-20 23:45:48 +0200

    Odabir slika se sada desava na kroz launchImageLibraryAsync na prvom ekranu. Slika se prosljedjuje kroz navigator. Edit sada ucitava pravu sliku. Spasavanje se treba desavati nakon pritiska na "Confirm" na EditScreenu. Spremno, save jos ne radi. PostEditScreen i SaveImageScreen izbrisani jer vise nisu potrebni. Meni komponenta prebacena u zaseban fajl. Dodani folder "./components" i fajl "./Components.js".

--- ImageFilter/Screens.js
+++ ImageFilter/Screens.js
@@ -1,8 +1,9 @@
 import HomeScreen from './screens/HomeScreen';
-import ImageSelectionScreen from './screens/ImageSelectionScreen';
 import EditScreen from './screens/EditScreen';
-import PostEditScreen from './screens/PostEditScreen';
-import SaveImageScreen from './screens/SaveImageScreen';
 import ShareScreen from './screens/ShareScreen';
 
-export { HomeScreen, ImageSelectionScreen, EditScreen, PostEditScreen, SaveImageScreen, ShareScreen };
+export { 
+    HomeScreen, 
+    EditScreen,
+    ShareScreen 
+};
